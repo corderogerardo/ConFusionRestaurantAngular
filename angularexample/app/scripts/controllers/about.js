0d028933375994985d6f6c8e69dcb760dd595908
Gerardo Cordero <cordero.gerard@gmail.com>
2016-01-01 19:38:59 -0430

    week 4 angular e2e testing, yo and yeoman, angular generator, for future projects.

---
+++ angularexample/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularexampleApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularexampleApp
+ */
+angular.module('angularexampleApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ angularexample/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularexampleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularexampleApp
+ */
+angular.module('angularexampleApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
